51a11c0180f35f48f4feffa0c5739eca5ada4259
nidhishree004 <shrinid20@gmail.com>
2024-11-09 16:36:05 +0530

    Resolved conflicts after pulling remote changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+<<<<<<< HEAD
 // src/App.js
 import React from 'react';
 import './styles.css';
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> fb985e2 (Initialize project using Create React App)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <h1>React App with CI/CD Pipeline</h1>
       <p>This React app is deployed using Jenkins, Docker, and Kubernetes.</p>
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> fb985e2 (Initialize project using Create React App)
     </div>
   );
 }
